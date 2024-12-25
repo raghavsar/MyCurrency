1d36d29c5ea8b77f8e28ee168e0fceaa5f713175
Raghav Sarmukaddam <93043660+raghavsar@users.noreply.github.com>
2024-12-25 11:02:08 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from fx_rates.views import CurrencyViewSet,CurrencyExchangeRateViewSet
-
+#here I am making some changes to generate PR #2
 app_name = 'mycurrency'
 
 router = DefaultRouter()
